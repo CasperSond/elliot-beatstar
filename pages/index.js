fe8250f5aad107453ead8d9ca867e94a73b7d373
Casper Sondergaard <casper.sndergaard@gmail.com>
2022-08-10 21:33:48 +0200

    change

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js! yeah</a>
+          Welcome to <a href="https://nextjs.org">Next.js! yeah 2</a>
         </h1>
 
         <p className={styles.description}>
